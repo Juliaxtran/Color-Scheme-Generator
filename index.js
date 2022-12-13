8d39288ebf530b0e025dc1ab2a7c3e93428ad6fe
Julia Tran <julia.kim.tran@hotmail.com>
2022-12-12 23:17:03 -0500

    Api call

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const colorInput = document.getElementsByClassName("color");
-const modeInput = document.getElementsByClassName("mode");
+const colorInput = document.getElementById("color");
+const modeInput = document.getElementById("mode");
+const url = "https://www.thecolorapi.com"
+
+
+
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const color = colorInput.value;
+  const color = colorInput.value.substring(1);
   console.log('color: ', color);
-  const options = modeInput.value;
-  console.log('options: ', options);
+  const mode = modeInput.value;
+  console.log('options: ', mode);
+  getColor();
 })
+
+const getColor = () => {
+  fetch(`${url}/scheme?hex=${color}&mode=${mode}&count=5`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => console.log(err));
+  }
+
+    // fetch(`${url}/scheme?hex=00000&mode=monochrome&count=5`)
